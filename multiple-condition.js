be959ed170b78cb5f7abf40ae06d68d5d74dbdf9
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-02 12:48:56 +0600

    first commit

---
+++ multiple-condition.js
@@ -0,0 +1,103 @@
+//                   && || operation 
+
+
+
+
+
+
+// 01. bassically ei code ta te amra duita sorto ei fulfill korte hobe tahole sotto hobe 
+// // je konon ekta mittha hole seta else voption e jabe . && diye duita sorto ke bujay 
+
+
+
+
+// var gotjob = true;
+// var moneySaved = 350000;
+
+// if (gotjob == true && moneySaved > 200000) {
+
+//     console.log('colo biye kore feli');
+// }
+// else {
+
+//     console.log(' Najmus Sakib tor kopal e biye nai ');
+// }
+
+
+
+
+
+
+
+
+// 02. 
+
+// var gotjob = true;
+// var moneysaved = 200000;
+// var hasflat = true;
+
+
+// eikhane e 3 ta sorto fulfill hoile seta sotto hobe ...je kono ekta full true na hole seta else option e jabe ;;;;;;;;;;;;;;;
+
+// if (gotjob == true && moneysaved > 100000 && hasflat == true) {
+
+//     console.log('tor kopal e biye ache ');
+// }
+// else {
+//     console.log('tor kopal e biye naiiiiiiii!!!!');
+// }
+
+
+
+
+
+
+
+
+
+// // 03
+
+// var gotjob = false;
+// var moneysaved = 200000;
+
+// // eikhane e 3 ta sorto fulfill hoile seta sotto hobe ...je kono ekta full true na hole seta else option e jabe ;;;;;;;;;;;;;;;
+
+// if (gotjob == true || moneysaved > 200000) {
+
+//     console.log('tor kopal e biye ache ');
+// }
+// else {
+//     console.log('tor kopal e biye naiiiiiiii!!!!');
+// }
+
+
+
+
+
+
+
+
+// 04 AND OR operation complex 
+//  zeikhane ekta sorto puron korlei true hobe 
+
+var gotjob = true;
+var moneySave = 700000;
+var hashouse = false;
+if ((gotjob == true && moneySave > 600000) || hashouse == true) {
+    console.log('colo biye kore feli');
+}
+else {
+    console.log('tor kopal e bityeee are  nai..kora lagbe nahh baca...!!')
+}
+
+
+
+
+
+
+
+
+
+
+
+
